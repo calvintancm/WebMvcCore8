2241adc1c741bde50674766c6201a4e7ba9eb2b7
calvintancm <calvin.tan@ptclogistics.com.sg>
2026-04-02 10:31:36 +0800

    Initial commit

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace ptc_IGH_Sys.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
